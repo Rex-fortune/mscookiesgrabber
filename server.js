ee859d83c8ae573e330243013429199d10084c5c
Rex <akpoyibofortune@gmail.com>
2025-01-14 14:34:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get("/", (req, res) => {
   console.log("cookies:", req.headers.cookie);
 });
 
+//allows cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Intercept and proxy requests to Microsoft
 // Log incoming requests for debugging
 app.use((req, res, next) => {
